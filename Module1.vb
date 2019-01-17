702acccc269e1a0eaf0ee7fac37e888a8deeeede
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-01-17 12:08:52 -0400

    FirstProject

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        'Dim firstName, lastName, fullName As String
+        'firstName = "Aitezaz"
+        'lastName = "Siddiqi"
+
+        'fullName = firstName & " " & lastName
+
+        'Console.WriteLine(fullName)
+
+        Dim x, y, z As Integer
+        x = 100
+        x = x + 7
+        y = 10
+        y = y - 2
+        z = x * (x + 2 * y) + y ^ 2 + 1001
+
+        Console.WriteLine(z)
+
+    End Sub
+
+End Module
