4d4ee3ed8406c656641c9ab3ba720993cb58ddc2
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-02-08 00:10:08 -0400

    Add files via upload

---
+++ Module3.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+    Sub main()
+        Dim year As Integer
+        year = 2019
+        While (year <= 2043)
+            If year Mod 100 = 0 AndAlso year Mod 400 = 0 Then
+                Console.WriteLine(year)
+            ElseIf year Mod 4 = 0 Then
+                Console.WriteLine(year)
+            End If
+            year += 1
+        End While
+    End Sub
+End Module
